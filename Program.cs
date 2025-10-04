5caea79bb51e7123c2410e6278344eb7c3f81d83
Matheus <ma8915004@gmail.com>
2025-10-03 22:01:43 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Digite seu Nome: ");
+string nome = Console.ReadLine()!;
+Console.Write("Digite seu Sobrenome: ");
+string sobrenome = Console.ReadLine()!;
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
